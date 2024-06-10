f98e936d3dc370067bf492150aff47dc6052318c
leticialima222 <leticiaakiralele2018@gmail.com>
2024-06-10 01:11:15 +0000

    style: estilização do h1 e do p do card

--- src/pages/Home.style.js
+++ src/pages/Home.style.js
@@ -4,6 +4,20 @@ export const CardContainer = styled.div`
     background: ${props => props.theme.cardGradient};
     max-width: 41.2rem;
     padding: 3.2rem;
+    border-radius: 3rem;
+
+    h1 {
+        font-size: 2.8rem;
+        color: ${props => props.theme.white};
+        margin-bottom: 3.5rem;
+        text-align: center;
+    }
+
+    p {
+        font-size: 3.5rem;
+        line-height: 2.4rem;
+        color: ${props => props.theme.textColor};
+    }
 `
 
 export const IconContainer = styled.div``
