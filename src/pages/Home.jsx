ee2955f1f16c2c04e4ec43f9bdf093b9fcde9720
leticialima222 <leticiaakiralele2018@gmail.com>
2024-06-10 02:02:05 +0000

    style: estilização concluida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,44 @@
-import { CardContainer, IconContainer } from "./Home.style";
+import { CardContainer, IconContainer, ButtonContainer, ResultContainer } from "./Home.style";
 
 import iconStarImg from "../assets/icon-star.svg"
-import { ButtonContainer } from "./Home.style";
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+
+
   return (
-    <CardContainer>
-      <IconContainer>
-        <img src={iconStarImg} alt="icone de estrela" />
-      </IconContainer>
+    aparecerResultado === false ? (
+      <CardContainer>
+        <IconContainer>
+          <img src={iconStarImg} alt="icone de estrela" />
+        </IconContainer>
 
-      <h1>Como foi o atendimento?</h1>
-      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feesback para podermos melhorar nosso atendimento!</p>
+        <h1>Como foi o atendimento?</h1>
+        <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feesback para podermos melhorar nosso atendimento!</p>
 
-      <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-      </ButtonContainer>
+        <ButtonContainer>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </ButtonContainer>
 
-      <button>Enviar</button>
+        <button>Enviar</button>
     </CardContainer>
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
+
+        <ResultContainer>
+          <p>você selecionou 4 de 5</p>
+        </ResultContainer>
+
+        <h1>Obrigado!</h1>
+
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte, não hesite em entrar em contato!</p>
+      </CardContainer>
+    ) 
   )
 }
